6e79e8f0015792b49e1ef8f2e59d9a60ab148bcb
Chad Fawcett <me@chadf.ca>
2015-05-06 00:31:26 -0700

    Eww, semi-colons

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 angular.module('tire.compare', ['ngResource'])
   .factory('TireResource', function($resource) {
-    var TireResource = $resource('http://localhost:3000/tires');
+    var TireResource = $resource('http://localhost:3000/tires')
 
-    return TireResource;
+    return TireResource
   })
   .controller('TireController', function($scope, TireResource) {
-    $scope.tires = TireResource.query();
+    $scope.tires = TireResource.query()
   })
