9e8201107b1959f817ec0933bae2dc760f740114
Chad Fawcett <me@chadf.ca>
2015-05-05 23:25:06 -0700

    App should do a basic query for tires

---
+++ index.js
@@ -0,0 +1,9 @@
+angular.module('tire.compare', ['ngResource'])
+  .factory('TireResource', function($resource) {
+    var TireResource = $resource('http://localhost:3000/tires');
+
+    return TireResource;
+  })
+  .controller('TireController', function($scope, TireResource) {
+    $scope.tires = TireResource.query();
+  })
