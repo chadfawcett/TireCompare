364ddc30b509fcadd2c1a6cba2b3fbb59c7d0f9f
Chad Fawcett <me@chadf.ca>
2015-09-04 21:11:37 -0700

    Filter tires by search params

--- index.js
+++ index.js
@@ -7,4 +7,26 @@ angular.module('tire.compare', ['ngResource'])
   .controller('TireController', function($scope, TireResource) {
     $scope.tires = TireResource.query()
     $scope.range = new Array(4)
+    $scope.searchParams = {
+      ratings: [
+        {'class': 'street', 'minStars': 0},
+        {'class': 'heavy rock', 'minStars': 0},
+        {'class': 'sand', 'minStars': 0},
+        {'class': 'snow', 'minStars': 0},
+        {'class': 'mud', 'minStars': 0},
+        {'class': 'ice', 'minStars': 0}
+      ]
+    }
+
+
+    $scope.tireFilter = function(tire) {
+      if (tire === undefined) return;
+
+      return tire.ratings[0].stars >= $scope.searchParams.ratings[0].minStars &&
+        tire.ratings[1].stars >= $scope.searchParams.ratings[1].minStars &&
+        tire.ratings[2].stars >= $scope.searchParams.ratings[2].minStars &&
+        tire.ratings[3].stars >= $scope.searchParams.ratings[3].minStars &&
+        tire.ratings[4].stars >= $scope.searchParams.ratings[4].minStars &&
+        tire.ratings[5].stars >= $scope.searchParams.ratings[5].minStars;
+    }
   })
